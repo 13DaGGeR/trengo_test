153949af09da35569533e76bc4df206dec93d19d
13dagger <13dagger@gmail.com>
2022-06-10 18:06:48 +0300

    feat: article model

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(3, true),
+            'body' => $this->faker->text,
+            'rating_score' => $this->faker->randomFloat(5, 0, 999),
+        ];
+    }
+}
