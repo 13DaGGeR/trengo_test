3e21ceabd5c34f52b27e3e733ab5797235677ecb
13dagger <13dagger@gmail.com>
2022-06-11 09:54:49 +0300

    feat: get categories test

---
+++ tests/Feature/GetCategoriesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetCategoriesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testHappyPath(): void
+    {
+        $categories = Category::factory(2)->create()->all();
+        $categoriesAsArray = [];
+        foreach ($categories as $category) {
+            $categoriesAsArray[] = [
+                'id' => $category->id,
+                'title' => $category->title,
+            ];
+        }
+
+        $this->get('/categories/')
+            ->assertStatus(200)
+            ->assertJson($categoriesAsArray, true);
+    }
+}
